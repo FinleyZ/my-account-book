b00514c94f9f5c34e4a1e657615a3f7efb09a7e0
Finley <zhf79212@gmail.com>
2020-11-17 21:41:31 -0330

    main page layput finished

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import './App.css';
 import Container from 'react-bootstrap'
 
 import MyAccountBook from './containers/MyAccountBook/MyAccountBook'
-
+import Navigation from './components/Navbar/Navbar'
 
 function App() {
   return (
     <div className="App">
+      <Navigation/>
         <MyAccountBook/>
 
     </div>
--- src/containers/MyAccountBook/MyAccountBook.js
+++ src/containers/MyAccountBook/MyAccountBook.js
@@ -1,15 +1,24 @@
 import React from 'react'
-import {Container} from 'react-bootstrap'
+import {Container, Row, Col} from 'react-bootstrap'
 import Navigation from '../../components/Navbar/Navbar'
-import Classes from './MyAccountBook'
 
 const MyAccountBook = () =>{
 	
 	return(
-		// <Container className={Classes.Container} fluid >
-			<Navigation/>
-		// </Container>
-		
+		<Container fluid className="h-92">
+			<Row className="h-100">
+				<Col className="bg-primary" lg="3" >
+				
+				</Col>
+				<Col 
+				className="bg-light" lg="9" style={{borderRadius: "2rem 2rem 0 0"}}>
+				
+				</Col>
+			</Row>
+
+
+
+		</Container>
 	)
 }
 
