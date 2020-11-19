b5422dd4b171d8c99ba6381882169be5c255a1c3
libinwen <libin.wen@nleats.com>
2020-11-18 21:40:45 -0500

    Small change
    ...

--- src/containers/MyAccountBook/MyAccountBook.js
+++ src/containers/MyAccountBook/MyAccountBook.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Container, Row, Col} from 'react-bootstrap'
-import AddSpending from '../../components/ControlComponent/AddSpending/AddSpending'
+import ControlComponent from '../../components/ControlComponent/ControlComponent'
 import DataDisplay from '../../components/DataDisplay/DataDisplay'
 import Navigation from '../../components/Navbar/Navbar'
 const MyAccountBook = () =>{
@@ -11,7 +11,7 @@ const MyAccountBook = () =>{
 			<Navigation/>
 			<Row className="h-100">
 				<Col className="bg-primary" lg="3" style={{minHeight: "500px"}}>
-				<AddSpending></AddSpending>
+				<ControlComponent />
 				</Col>
 
 				<Col className="bg-light" lg="9" style={{borderRadius: "2rem 2rem 0 0"} }>
