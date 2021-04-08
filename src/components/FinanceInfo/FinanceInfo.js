0adae212d2b60adc2c40dca4512ee862620af575
Yahia-Aly <56008993+Yahia-Aly@users.noreply.github.com>
2021-04-08 03:14:00 +0200

    Update Finance Info colors

--- src/components/FinanceInfo/FinanceInfo.js
+++ src/components/FinanceInfo/FinanceInfo.js
@@ -12,13 +12,13 @@ export const FinanceInfo = (props) => {
 
 
 			<Row className="h-25" lg="6">
-				<Col  className="bg-info " lg="4" md="3" style={{minHeight: "50px", fontSize: 18, opacity: 0.75}}>My asset </Col>
-				<Col className="bg-info " lg="4" md="3" style={{minHeight: "50px", fontSize: 18,  opacity: 0.75}}>My debt </Col>
+				<Col  className="bg-warning " lg="4" md="3" style={{minHeight: "50px", fontSize: 18, opacity: 0.75}}>My asset </Col>
+				<Col className="bg-warning " lg="4" md="3" style={{minHeight: "50px", fontSize: 18,  opacity: 0.75}}>My debt </Col>
 
 			</Row>
 			<Row className="h-25" lg="3">
-				<Col  className="bg-info " lg="4" md="3" style={{minHeight: "50px", fontSize: 22, color: 'white'}}>$ {asset}</Col>
-				<Col className="bg-info " lg="4" md="3" style={{minHeight: "50px", fontSize: 22,color: 'white'}}>$ {debt} </Col>
+				<Col  className="bg-dark " lg="4" md="3" style={{minHeight: "50px", fontSize: 22, color: 'white'}}>$ {asset}</Col>
+				<Col className="bg-dark " lg="4" md="3" style={{minHeight: "50px", fontSize: 22,color: 'white'}}>$ {debt} </Col>
 
 			</Row>
 
